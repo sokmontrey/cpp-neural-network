d295b05219659bf08d8425a3627492feeba32ab6
Sokmontrey <sythatsokmontrey@gmail.com>
2022-03-29 09:41:47 -0400

    use activated as the params for derivative

--- src/Activation/activation.cpp
+++ src/Activation/activation.cpp
@@ -10,14 +10,12 @@ double Activation::relu(double x){
     return max(0.0, x);
 }
 
-double Activation::dSigmoid(double x){
-    double activated = Activation::sigmoid(x);
+double Activation::dSigmoid(double activated){
     return activated * (1.0 - activated);
 }
-double Activation::dTanh(double x){
-    double activated = Activation::tanh(x);
+double Activation::dTanh(double activated){
     return 1.0 - pow(activated, 2);
 }
-double Activation::dRelu(double x){
-    return (x>0 ? 1 : 0);
+double Activation::dRelu(double activated){
+    return (activated>0 ? 1 : 0);
 }
