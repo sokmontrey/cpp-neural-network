ddbc914f4659a226c558524464280db4f735c032
Sokmontrey <sythatsokmontrey@gmail.com>
2022-03-29 18:49:39 -0400

    g++ compile command

--- src/index.cpp
+++ src/index.cpp
@@ -3,6 +3,5 @@
 int main(){
 	Matrix m(3,3,-1, 1, 1);
 	m.print();
-	//use g++ to compile this file and all the files in the Matrix folder and Activation folder
-	//g++ index.cpp -I./Matrix -I./Activation
+	//g++ index.cpp ./Matrix/*.cpp ./Activation/*.cpp
 }
