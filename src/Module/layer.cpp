96447c477fd6810cc2eb57db5fadc38b827520c2
Sokmontrey <sythatsokmontrey@gmail.com>
2022-03-29 12:13:01 -0400

    defined basic of Layer constructor

--- src/Module/layer.cpp
+++ src/Module/layer.cpp
@@ -1,3 +1,8 @@
 #include "layer.hpp"
 
 Layer::Layer() = default;
+
+Layer::Layer(int inputLength, int outputLength, Matrix (*activationFunc)(Matrix& a), (*dActivationFunc)(Matrix& activated)){
+  this->weight = new Matrix(inputLength, outputLength, -1, 1, 2);
+  this->bias = new Matrix(outputLength, 1, -1, 1, 2);
+}
