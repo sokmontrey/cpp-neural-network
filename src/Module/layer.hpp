2e37e5f5b49bcd7d8f38bc9d286cc47aca325d95
Sokmontrey <sythatsokmontrey@gmail.com>
2022-03-29 11:59:07 -0400

    declare constructor and getter.

--- src/Module/layer.hpp
+++ src/Module/layer.hpp
@@ -10,4 +10,15 @@ class Layer{
     Matrix weight;
     Matrix bias;
     Matrix output;
+    Matrix (*activationFunction)(Matrix& a);
+    Matrix (*dActivationFunction)(Matrix& activated);
+  
+  public:
+    Layer();
+    Layer(int inputLength, int outputLength, Matrix (*activationFunc)(Matrix& a));
+    
+    /*__getter__*/
+    Matrix getOutput();
+    Matrix getWeight();
+    Matrix getBias();
 };
