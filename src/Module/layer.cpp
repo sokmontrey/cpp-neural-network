e872b986606c5fff563def0aa61c5ffac55df0de
Sokmontrey <sythatsokmontrey@gmail.com>
2022-03-30 18:19:24 -0400

    modified initialize & create forward()

--- src/Module/layer.cpp
+++ src/Module/layer.cpp
@@ -3,15 +3,26 @@
 Layer::Layer() = default;
 
 Layer::Layer(
-  int inputLength, 
-  int outputLength, 
-  Matrix (*activationFunc)(Matrix& a), 
-  Matrix (*dActivationFunc)(Matrix& activated)
+	int inputLength, int outputLength, 
+	Matrix (*activationFunc)(Matrix& a), 
+	Matrix (*dActivationFunc)(Matrix& activated)
 ){
-  this->weight = Matrix(inputLength,outputLength,{-1,1,1});
-  this->bias = Matrix(outputLength,1,{-1,1,1});
-  this->output = Matrix(outputLength, 1, 0);
+	this->weight = Matrix(inputLength, outputLength, {-1, 1, 1});
+	this->bias = Matrix(1, outputLength, {-1, 1, 1});
+	this->output = Matrix(1, outputLength, 0);
   
-  this->activation = activationFunc;
-  this->dActivation = dActivationFunc;
+	this->activation = activationFunc;
+	this->dActivation = dActivationFunc;
 }
+
+Matrix Layer::forward(Matrix& input){
+	output = input.matmul(weight) + bias;
+	output = activation(output);
+	return output;
+}
+Matrix Layer::forward(Matrix input){
+	output = input.matmul(weight) + bias;
+	output = activation(output);
+	return output;
+}
+
