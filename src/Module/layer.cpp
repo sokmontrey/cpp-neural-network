f03a1453eed6e7b8e62ce36686e1fdee7f389c32
Sokmontrey <sythatsokmontrey@gmail.com>
2022-03-30 12:37:08 -0400

    assign act and dAct func

--- src/Module/layer.cpp
+++ src/Module/layer.cpp
@@ -11,4 +11,7 @@ Layer::Layer(
   this->weight = Matrix(inputLength,outputLength,{-1,1,1});
   this->bias = Matrix(outputLength,1,{-1,1,1});
   this->output = Matrix(outputLength, 1, 0);
+  
+  this->activation = activationFunc;
+  this->dActivation = dActivationFunc;
 }
