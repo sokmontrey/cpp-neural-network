ca3355586d41adb1532b7b1a1b0839f6622fbaa6
Sokmontrey <sythatsokmontrey@gmail.com>
2022-04-02 12:20:54 -0400

    test

--- src/index.cpp
+++ src/index.cpp
@@ -1,10 +1,23 @@
 #include "./Matrix/matrix.hh"
 #include "./Activation/activation.hh"
 #include "./Model/layer.hh"
+#include "./Model/model.hh"
 
 int main(){
 	//g++ index.cpp ./Matrix/*.cpp ./Activation/*.cpp
 	
-	Layer l(2,3, Activation::matrixRelu);
-	l.forward({{1,2}}).print();
+	//Model m({2,3,2}, {Activation::matrixSigmoid, Activation::matrixSigmoid});
+	Model m({
+			Layer(2),
+			Layer(2,3),
+			Layer(3,2)
+			});
+
+	m.setActivation({Activation::matrixSigmoid, Activation::matrixSigmoid});
+	m.print();
+	cout << "\n-----------------\n";
+	m.forward(Matrix({{1,1}})).print();
+	//TODO: abiguis bug
+	cout << "\n-----------------\n";
+	m.print();
 }
