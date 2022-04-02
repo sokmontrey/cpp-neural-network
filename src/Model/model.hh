ea8d46ba81a0eb32527bcf112ad70bea4657c75b
Sokmontrey <sythatsokmontrey@gmail.com>
2022-04-02 10:57:45 -0400

    declare constructor and activation setter

--- src/Model/model.hh
+++ src/Model/model.hh
@@ -8,7 +8,17 @@
 #include "./layer.hh"
 
 class Model{
+	private:
+		vector<int> shape;
+		//including input layer
+		vector<Layer> layers;
+	public:
+		Model(vector<int> shape);
+		Model(vector<int> shape, vector<Matrix (*)(Matrix& a, bool isDerivative) activations>);
+		Model(vector<Layer> layers);
 
+		/*__setter__*/
+		void setActivation(vector<Matrix (*)(Matrix& a, bool isDerivative)> activations);
 };
 
 #endif
