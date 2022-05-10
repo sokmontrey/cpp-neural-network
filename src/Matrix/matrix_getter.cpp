4ff4aa71842730155974311fcbae1dd5c38e2f92
SokmontreySytha <>
2022-05-10 14:57:18 +0000

    rewrite the lib

--- src/Matrix/matrix_getter.cpp
+++
@@ -1,4 +0,0 @@
-#include "matrix.hh"
-
-int Matrix::getRows(){ return rows; }
-int Matrix::getCols(){ return cols; }
--- src/index.cpp
+++ src/index.cpp
@@ -1,23 +1,6 @@
-#include "./Matrix/matrix.hh"
-#include "./Activation/activation.hh"
-#include "./Model/layer.hh"
-#include "./Model/model.hh"
-#include "./Loss/loss.hh"
+#include <iostream>
+using namespace std;
 
 int main(){
-	//g++ index.cpp ./Matrix/*.cpp ./Activation/*.cpp
-	
-	//Model m({2,3,2}, {Activation::matrixSigmoid, Activation::matrixSigmoid});
-	Model m({
-			Layer(2),
-			Layer(2,3),
-			Layer(3,2)
-			});
-
-	m.setActivation({Activation::matrixSigmoid, Activation::matrixSigmoid});
-	m.setLoss(Loss::mse);
-	m.forward({{1,2}}).print();
-	m.getOutput().print();
-	cout << m.loss(Matrix({{1,1}})) << endl;
-	m.matrixLoss(Matrix({{1,1}})).print();
+	cout << "Hello world";
 }
