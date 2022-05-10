b60e550e835ec51c6356fa19d2c6e3b2946174fa
Sokmontrey <sythatsokmontrey@gmail.com>
2022-05-10 18:13:55 +0000

    matrix

--- src/index.cpp
+++ src/index.cpp
@@ -8,9 +8,5 @@ using namespace std;
 int main(){
 	Matrix a(vector<vector<double>> {{1,2,3}, {4,5,6}});
 	Matrix b(vector<vector<double>>{{1,2,3}, {1,2,3}});
-	Matrix::add(a, b).print();
-	Matrix::mul(a, b).print();
-	Matrix::add(a, 10).print();
-	Matrix::mul(b, 10).print();
 	cout << endl;
 }
