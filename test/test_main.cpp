0c9b15fe66b0bd76d6b13f32c4770674eb36d203
Sokmontrey <sythatsokmontrey@gmail.com>
2023-08-31 23:51:36 -0400

    setup third_party

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,48 @@
 #include <gamma_net/gamma_net.h>
+#include <nodeflow/nodeflow.h>
+#include <matplot/matplot.h>
+#include <Eigen/Dense>
+#include <vector>
+
+using namespace nodeflow;
+namespace plt = matplot;
 
 int main(){
-    f();
+    int layer_size = 4;
+    int layer[] = {1, 4, 4, 1};
+
+    Node x = Node::Constant(layer[0], 1, 1);
+
+    std::vector<Node> w;
+    std::vector<Node> b;
+
+    for(int i=0; i<layer_size-1; i++){
+        w.push_back(Node::Random(layer[i+1], layer[i]));
+        b.push_back(Node::Random(layer[i+1]));
+    }
+
+    Mul m1{&w[0], &x};
+    Add z1{&m1, &b[0]};
+    Tanh h1{&z1};
+
+    Mul m2{&w[1], &h1};
+    Add z2{&m2, &b[1]};
+    Tanh h2{&z2};
+
+    Mul m3{&w[2], &h2};
+    Add z3{&m3, &b[2]};
+    Tanh o{&z3};
+
+    o.finished();
+
+    o.forward();
+    o.print();
+    o.reset();
+
+    x(0,0) = 10;
+
+    o.forward();
+    o.print();
+
     return 0;
 }
